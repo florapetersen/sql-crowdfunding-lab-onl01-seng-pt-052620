5e4f341c4bd362aef09c6436d92d820fb52bf0de
Flora Petersen <floramarina@comcast.net>
2020-08-04 20:34:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,10 +2,17 @@ INSERT INTO users (name, age) VALUES ("Flora", 29), ("Marina", 29), ("Lucia", 34
 ("Michael", 68), ("Theano", 67), ("Mouratides", 67), ("Katie", 30), ("Jeanne", 30), ("Stephanie", 29), ("Nicole", 29),
 ("Kate", 36), ("Skye", 36), ("Eugenie", 33), ("Aimee", 34), ("Karisse", 34), ("Ashley", 33), ("Lily", 26), ("Malia", 27);
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("A", "fun", 100, 10/19/20, 11/19/20), ("B", "help", 200, 8/5/20, 8/6/20),
-("C", "charity", 500, 9/1/20, 9/15/20), ("D", "fun", 10000, 8/15/20, 9/16/20), ("E", "emergency", 25000, 8/6/20, 11/6/20),
-("F", "fun", 900, 8/27/20, 9/20/20), ("G", "fun", 11000, 8/10/20, 9/10/20), ("H", "help", 250, 8/29/20, 10/29/20), ("I", "emergency", 100000, 8/14/20, 12/14/20),
-("J", "award", 560, 8/10/20, 10/20/20);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+(2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
+(3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
+(4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30'),
+(5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'),
+(6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30'),
+(7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
+(8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
+(9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
+(10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
 
 INSERT INTO pledges (amount, user_id, project_id) VALUES (5.00, 1, 2), (5.00, 10, 8), (5.00, 12, 8),
 (8.00, 1, 3),
